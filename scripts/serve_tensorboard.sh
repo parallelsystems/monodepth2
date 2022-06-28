707dbeacc701d934fc2ecd2b093b300d23f4c63b
Jeffrey Hu <jeffrey@moveparallel.com>
2022-06-28 14:53:03 -0700

    training on smol dataset

---
+++ scripts/serve_tensorboard.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+tensorboard --logdir ./output/ --host 0.0.0.0
---
+++ scripts/train_oxnard.sh
@@ -0,0 +1,2 @@
+
+python train.py --model_name mono_model --dataset oxnard --data_path=/home/jeffrey/Parallel/railyard/data/datasets/triplets_daytime --height 416 --width 608 --png --batch_size 4 --logdir ./output
